3ebcac19d3073c0d3d8c814ed9889b67f279f01a
RaulGonzalezSendon <raulgonzalezsendon2003@gmail.com>
2021-08-10 10:20:41 +0200

    Rating Total de todos los equipos

--- Assets/SCRIPT/READ_SCRIPT.cs
+++ Assets/SCRIPT/READ_SCRIPT.cs
@@ -17,18 +17,18 @@ public class READ_SCRIPT : MonoBehaviour
     public Image club;
     public GameObject Escudos;
 
-    public CARD[] Real_Madrid;
-    public CARD[] Arsenal;
-    public CARD[] Tottenham;
-    public CARD[] Chelsea;
-    public CARD[] Atletico_de_Madrid;
-    public CARD[] Juventus;
-    public CARD[] Milan;
-    public CARD[] Manchester_City;
-    public CARD[] Liverpool;
-    public CARD[] Manchester_United;
-    public CARD[] Barcelona;
-    public CARD[] Inter_de_Milan;
+    public CARD[] Real_Madrid; public float RatingRealMadrid;
+    public CARD[] Arsenal; public float RatingArsenal;
+    public CARD[] Tottenham; public float RatingTottenham;
+    public CARD[] Chelsea; public float RatingChelsea;
+    public CARD[] Atletico_de_Madrid; public float RatingAtleticoDeMadrid;
+    public CARD[] Juventus; public float RatingJuventus;
+    public CARD[] Milan; public float RatingMilan;
+    public CARD[] Manchester_City; public float RatingManchesterCity;
+    public CARD[] Liverpool; public float RatingLiverpool;
+    public CARD[] Manchester_United; public float RatingManchesterUnited;
+    public CARD[] Barcelona; public float RatingBarcelona;
+    public CARD[] Inter_de_Milan; public float RatingInterDeMilan;
     public CARD_DISPLAY[] cards;
 
     public Spawn_Balones PowerUps;
@@ -43,7 +43,7 @@ public class READ_SCRIPT : MonoBehaviour
     }
     private void Update()
     {
-        
+
     }
     public void PowerUp()
     {
@@ -78,7 +78,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Real_Madrid[i]);
                 i++;
-                Real_Madrid[0].Rating;
+            }
+            for (int RealMadrid = 0; RealMadrid <= 10; RealMadrid++)
+            {
+                RatingRealMadrid = RatingRealMadrid + Real_Madrid[RealMadrid].Rating;
             }
         }
         if (clubNameSaved == "Arsenal")
@@ -89,6 +92,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Arsenal[i]);
                 i++;
             }
+            for (int Ars = 0; Ars <= 10; Ars++)
+            {
+                RatingArsenal = RatingArsenal + Arsenal[Ars].Rating;
+            }
         }
         if (clubNameSaved == "Tottenham")
         {
@@ -97,6 +104,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Tottenham[i]);
                 i++;
+            }
+            for (int Tot = 0; Tot <= 10; Tot++)
+            {
+                RatingTottenham = RatingTottenham + Tottenham[Tot].Rating;
             }
         }
         if (clubNameSaved == "Chelsea")
@@ -107,6 +118,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Chelsea[i]);
                 i++;
             }
+            for (int Che = 0; Che <= 10; Che++)
+            {
+                RatingChelsea = RatingChelsea + Chelsea[Che].Rating;
+            }
         }
         if (clubNameSaved == "Atletico_de_Madrid")
         {
@@ -115,6 +130,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Atletico_de_Madrid[i]);
                 i++;
+            }
+            for (int Atl = 0; Atl <= 10; Atl++)
+            {
+                RatingAtleticoDeMadrid = RatingAtleticoDeMadrid + Atletico_de_Madrid[Atl].Rating;
             }
         }
         if (clubNameSaved == "Juventus")
@@ -125,6 +144,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Juventus[i]);
                 i++;
             }
+            for (int Juv = 0; Juv <= 10; Juv++)
+            {
+                RatingJuventus = RatingJuventus + Juventus[Juv].Rating;
+            }
         }
         if (clubNameSaved == "Milan")
         {
@@ -133,6 +156,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Milan[i]);
                 i++;
+            }
+            for (int Mil = 0; Mil <= 10; Mil++)
+            {
+                RatingMilan = RatingMilan + Milan[Mil].Rating;
             }
         }
         if (clubNameSaved == "Manchester_City")
@@ -143,6 +170,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Manchester_City[i]);
                 i++;
             }
+            for (int ManC = 0; ManC <= 10; ManC++)
+            {
+                RatingManchesterCity = RatingManchesterCity + Manchester_City[ManC].Rating;
+            }
         }
         if (clubNameSaved == "Liverpool")
         {
@@ -151,6 +182,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Liverpool[i]);
                 i++;
+            }
+            for (int Liv = 0; Liv <= 10; Liv++)
+            {
+                RatingLiverpool = RatingLiverpool + Liverpool[Liv].Rating;
             }
         }
         if (clubNameSaved == "Manchester_United")
@@ -161,6 +196,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Manchester_United[i]);
                 i++;
             }
+            for (int ManU = 0; ManU <= 10; ManU++)
+            {
+                RatingManchesterUnited = RatingManchesterUnited + Manchester_United[ManU].Rating;
+            }
         }
         if (clubNameSaved == "Barcelona")
         {
@@ -170,6 +209,10 @@ public class READ_SCRIPT : MonoBehaviour
                 item.ChangePlayer(Barcelona[i]);
                 i++;
             }
+            for (int Bar = 0; Bar <= 10; Bar++)
+            {
+                RatingBarcelona = RatingBarcelona + Barcelona[Bar].Rating;
+            }
         }
         if (clubNameSaved == "Inter_de_Milan")
         {
@@ -178,6 +221,10 @@ public class READ_SCRIPT : MonoBehaviour
             {
                 item.ChangePlayer(Inter_de_Milan[i]);
                 i++;
+            }
+            for (int Int = 0; Int <= 10; Int++)
+            {
+                RatingInterDeMilan = RatingInterDeMilan + Inter_de_Milan[Int].Rating;
             }
         }
     }
